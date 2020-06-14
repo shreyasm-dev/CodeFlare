5c41aea22e156074924a10b30f9fb9983801d9c7
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2020-06-14 10:10:15 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -53,6 +53,11 @@ require(["vs/editor/editor.main"], function() {
   }
 });
 
+console.log = function(log) {
+  var consoleDiv = document.createElement('div')
+  consoleDiv.innerHTMl = log
+}
+
 function toggleSandbox() {
   if (document.querySelector('iframe').hasAttribute('sandbox')) {
     document.querySelector('iframe').removeAttribute('sandbox')
@@ -68,6 +73,8 @@ function getSandbox() {
     return false
   }
 }
+
+//toggleSandbox()
 
 document.getElementById('customizeButton').addEventListener('click', function() {
   document.getElementById('customizeButtonOptions').classList.toggle('show')
@@ -86,85 +93,107 @@ document.getElementById('customizeDebugOptions').addEventListener('click', funct
 })
 
 const pickr = Pickr.create({
-    el: '#color-picker-theme',
-    theme: 'classic',
-		default: '#000000',
-		container: document.getElementById("themePicker"),
+  el: '#color-picker-theme',
+  theme: 'classic',
+  default: '#000000',
+  container: document.getElementById("themePicker"),
 
-    swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)'
-    ],
+  swatches: [
+    'rgba(244, 67, 54, 1)',
+    'rgba(233, 30, 99, 0.95)',
+    'rgba(156, 39, 176, 0.9)',
+    'rgba(103, 58, 183, 0.85)',
+    'rgba(63, 81, 181, 0.8)',
+    'rgba(33, 150, 243, 0.75)',
+    'rgba(3, 169, 244, 0.7)',
+    'rgba(0, 188, 212, 0.7)',
+    'rgba(0, 150, 136, 0.75)',
+    'rgba(76, 175, 80, 0.8)',
+    'rgba(139, 195, 74, 0.85)',
+    'rgba(205, 220, 57, 0.9)',
+    'rgba(255, 235, 59, 0.95)',
+    'rgba(255, 193, 7, 1)'
+  ],
 
-    components: {
+  components: {
 
-        // Main components
-        preview: true,
-        opacity: true,
-        hue: true,
+    // Main components
+    preview: true,
+    opacity: true,
+    hue: true,
 
-        // Input / output Options
-        interaction: {
-            hex: true,
-            rgba: true,
-            hsla: true,
-            hsva: true,
-            cmyk: true,
-            input: true,
-            clear: true,
-            save: true
-        }
+    // Input / output Options
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: true,
+      cmyk: true,
+      input: true,
+      clear: true,
+      save: true
     }
+  }
 });
 
 const pickrSecondary = Pickr.create({
-    el: '#color-picker-theme-secondary',
-    theme: 'classic',
-		default: '#ffffff',
-		container: document.getElementById("themePicker"),
+  el: '#color-picker-theme-secondary',
+  theme: 'classic',
+  default: '#ffffff',
+  container: document.getElementById("themePicker"),
 
-    swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)'
-    ],
+  swatches: [
+    'rgba(244, 67, 54, 1)',
+    'rgba(233, 30, 99, 0.95)',
+    'rgba(156, 39, 176, 0.9)',
+    'rgba(103, 58, 183, 0.85)',
+    'rgba(63, 81, 181, 0.8)',
+    'rgba(33, 150, 243, 0.75)',
+    'rgba(3, 169, 244, 0.7)',
+    'rgba(0, 188, 212, 0.7)',
+    'rgba(0, 150, 136, 0.75)',
+    'rgba(76, 175, 80, 0.8)',
+    'rgba(139, 195, 74, 0.85)',
+    'rgba(205, 220, 57, 0.9)',
+    'rgba(255, 235, 59, 0.95)',
+    'rgba(255, 193, 7, 1)'
+  ],
 
-    components: {
-        preview: true,
-        opacity: true,
-        hue: true,
-        interaction: {
-            hex: true,
-            rgba: true,
-            hsla: true,
-            hsva: true,
-            cmyk: true,
-            input: true,
-            clear: true,
-            save: true
-        }
+  components: {
+
+    // Main components
+    preview: true,
+    opacity: true,
+    hue: true,
+
+    // Input / output Options
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: true,
+      cmyk: true,
+      input: true,
+      clear: true,
+      save: true
     }
+  }
 });
+
+pickr.on('save', function(color) {
+  if (color != null) {
+    color = color.toRGBA()
+    document.querySelector('html').style.setProperty('--theme-color-', 'rgba(' + color[0] + ', ' + color[1] + ', ' + color[2] + ', ' + color[3] + ')')
+  } else {
+    document.querySelector('html').style.setProperty('--theme-color-', 'black')
+  }
+})
+
+pickrSecondary.on('save', function(color) {
+  if (color != null) {
+    color = color.toRGBA()
+    document.querySelector('html').style.setProperty('--theme-color-secondary-', 'rgba(' + color[0] + ', ' + color[1] + ', ' + color[2] + ', ' + color[3] + ')')
+  } else {
+    document.querySelector('html').style.setProperty('--theme-color-secondary-', 'black')
+  }
+})
